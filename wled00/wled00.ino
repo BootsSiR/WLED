71fae3affc1ad10ea68f6f87108528ba320b0696
Steve <steve@dogsbyte.net>
2021-04-20 08:51:34 -0300

    Code to disable brownout detection - not used

--- wled00/wled00.ino
+++ wled00/wled00.ino
@@ -12,7 +12,12 @@
  */
 #include "wled.h"
 
+//#include "soc/soc.h"
+//#include "soc/rtc_cntl_reg.h"
+
 void setup() {
+  //WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+
   WLED::instance().setup();
 }
 
